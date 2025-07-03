71c04a795bce97592c5c43362d725139fc1dab05
Vlad <koalah86@gmail.com>
2025-07-03 19:42:55 +0200

    fix: loading problem

--- src/components/_screen/home/index.tsx
+++ src/components/_screen/home/index.tsx
@@ -7,21 +7,26 @@ const HomeScreen = () => {
   const { progress } = useProgress();
   return (
     <div className="relative w-screen h-screen overflow-hidden">
-      <CanvasContainer />
-      <div>
-        <div className="fixed bottom-2 text-xs left-1/2 -translate-x-1/2">
-          Click anywhere to turn on turbo
-        </div>
-        <button
-          onClick={() => {
-            window.location.reload();
-          }}
-          className="fixed top-2 text-xs right-2 z-50 cursor-pointer"
-        >
-          <ArrowUturnLeftIcon width={24} height={24} />
-        </button>
-      </div>
-      {progress < 90 && <LoadingScreen />}
+      <>
+        {progress < 100 ? (
+          <LoadingScreen />
+        ) : (
+          <>
+            <CanvasContainer />
+            <div className="fixed bottom-2 text-xs left-1/2 -translate-x-1/2">
+              Click anywhere to turn on turbo
+            </div>
+            <button
+              onClick={() => {
+                window.location.reload();
+              }}
+              className="fixed top-2 text-xs right-2 z-50 cursor-pointer"
+            >
+              <ArrowUturnLeftIcon width={24} height={24} />
+            </button>
+          </>
+        )}
+      </>
     </div>
   );
 };
